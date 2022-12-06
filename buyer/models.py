06bcdcbeb30b4e7422da3d68a0dee9e7ddc4933d
saleha <salehashaikh410@gmail.com>
2022-12-06 15:18:22 +0530

    'register'

--- buyer/models.py
+++ buyer/models.py
@@ -1,3 +1,15 @@
 from email.policy import default
+from django.db import models
 
 # Create your models here.
+
+
+class Buyer(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    cpassword = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.firstname
--- buyer/views.py
+++ buyer/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
 from mysite import settings
+from .models import Buyer
 
 # Create your views here.
 
@@ -16,28 +17,56 @@ def registration(request):
     if request.method == 'GET':
         return render(request, 'registration_form.html')
     else:
-        global user_data
-        user_data = {
-            'first_name': request.POST['firstName'],
-            'last_name': request.POST['lastName'],
-            'email': request.POST['email'],
-            'password': request.POST['password'],
-            're_password': request.POST['cpassword'],
-        }
-        if user_data['password'] == user_data['re_password']:
-            global c_otp
-            c_otp = random.randint(1000, 9999)
-            message = f'Hi, your OTP is {c_otp}'
-            subject = 'Carpolling Registration,'
-            from_email = settings.EMAIL_HOST_USER
-            send_mail(subject, message, from_email, [user_data['email']])
-            return render(request, 'otp.html', {'msg': 'Check Your MailBox'})
+        if request.POST['password'] == request.POST['cpassword']:
+            password = request.POST['password']
+            global user_data
+            user_data = {
+                'first_name': request.POST['firstname'],
+                'last_name': request.POST['lastname'],
+                'email': request.POST['email'],
+                'password': request.POST['password'],
+                're_password': request.POST['cpassword'],
+            }
+            if len(password) >= 8:
+                special_char = False
+                numbers = False
+                upper_case = False
+                lower_case = False
+                for i in password:
+                    if i in '!@#$%^&*':
+                        special_char = True
+                    if i in '0123456789':
+                        numbers = True
+                    if 97 <= ord(i) <= 122:
+                        lower_case = True
+                    if 65 <= ord(i) <= 90:
+                        upper_case = True
+                if lower_case and upper_case and special_char and numbers:
+                    global c_otp
+                    c_otp = random.randint(1000, 9999)
+                    subject = "Registration At Blogspot"
+                    message = f"Your OTP is {c_otp}."
+                    from_email = settings.EMAIL_HOST_USER
+                    send_mail(subject, message, from_email,
+                              [request.POST['email']])
+                    return render(request, 'otp.html', {'msg': 'Check Your Inbox!!'})
+                else:
+                    return render(request, 'registration_form.html', {'msg': 'akjsdklasjd'})
         else:
-            return render(request, 'registration_form.html', {'msg': 'Passwords Does Not Match'})
+            return render(request, 'registration_form.html', {'msg': 'Both passwords are not same!!'})
 
 
 def otp(request):
-    return render(request, 'otp.html')
+    if request.method == 'POST':
+        if c_otp == int(request.POST['uotp']):
+            Buyer.objects.create(
+                firstname=user_data['first_name'],
+                lastname=user_data['last_name'],
+                email=user_data['email'],
+                password=user_data['password'],
+                cpassword=user_data['re_password'])
+            return render(request, 'registration_form.html', {'msg': 'Account Successfully created!!'})
+    return render(request, 'login.html')
 
 
 def about(request):
@@ -54,14 +83,6 @@ def faqs(request):
 
 def checkout(request):
     return render(request, 'checkout.html')
-
-
-def help(request):
-    return render(request, 'help.html')
-
-
-def icons(request):
-    return render(request, 'icons.html')
 
 
 def index(request):
@@ -94,10 +115,6 @@ def single2(request):
 
 def terms(request):
     return render(request, 'terms.html')
-
-
-def typography(request):
-    return render(request, 'typography.html')
 
 
 def home(request):
